c62ee40a6b58d4cf1c6f0a1b87e316c633574d84
boogyeom <swippylab.gyeom@gmail.com>
2022-05-17 20:19:12 +0900

    FEATURE: functions에서 firestore, storage에뮬레이터 접근
    functions에서 firestore, storage 에뮬레이터에 접근하는 코드 예시, .env 파일에 호스트 정보가 있다

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
+import 'dotenv/config';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+
 export * from './user/temp';
--- src/user/temp.ts
+++ src/user/temp.ts
@@ -1,6 +1,27 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
+export const helloFireStore = functions.https.onRequest(async (request, response) => {
+  admin
+    .firestore()
+    .collection('users')
+    .get()
+    .then((querySnapshot) => {
+      response.send('Hello from FireStore!');
+      querySnapshot.forEach((doc) => {
+        functions.logger.info(doc.data());
+      });
+    });
+});
+
+export const helloStroage = functions.https.onRequest(async (request, response) => {
+  admin
+    .storage()
+    .bucket('gs://swippylab-sopo.appspot.com')
+    .file('users/user1.jpg')
+    .get() //'users/user1.jpg')
+    .then((responseFile) => {
+      response.send('Hello from Storage');
+      functions.logger.info(responseFile[0].name);
+    });
 });
