192dfca26d2e4d1beb7303e9256e7f7baffcd5a3
David <david@swippylab.com>
2022-06-14 18:02:32 +0900

    RENAME: handle peding new posts to handle pending posts

--- src/post/pending_new_posts.ts
+++ src/post/pending_new_posts.ts
@@ -5,41 +5,39 @@ import { FIELD } from '../constant/field';
 import sendPostToUser from './send_post';
 
 const maxWaitHour = 2;
-const executionDelayHour = 4;
+const executionDelayHour = 2;
 
 const log = functions.logger;
 
-export const onShceduledHandlePendingNewPosts = functions.pubsub
+export const onShceduledHandlePendingPosts = functions.pubsub
   .schedule(`0 */${executionDelayHour} * * *`)
   // .schedule(`* * * * *`)
   .onRun(async (/* context */) => {
-    await handlePendingNewPosts();
+    await handlePendingPosts();
   });
 
-  export async function handlePendingNewPosts() {
-    log.debug('start schedule function');
+export async function handlePendingPosts() {
+  log.debug('start schedule function');
 
-    const firestore = admin.firestore();
+  const firestore = admin.firestore();
 
-    const pendNewPostsRef = firestore.collection(COLLECTION.PEDINGNEWPOSTS);
+  const pendNewPostsRef = firestore.collection(COLLECTION.PEDINGNEWPOSTS);
 
-    const searchLimitDate = new Date();
-    searchLimitDate.setHours(searchLimitDate.getHours() - maxWaitHour);
+  const searchLimitDate = new Date();
+  searchLimitDate.setHours(searchLimitDate.getHours() - maxWaitHour);
 
-    log.debug(`search limit time : ${searchLimitDate.toString()}`);
+  log.debug(`search limit time : ${searchLimitDate.toString()}`);
 
-    const querySnapshot = await pendNewPostsRef.where(FIELD.DATE, '<=', searchLimitDate).get();
+  const querySnapshot = await pendNewPostsRef.where(FIELD.DATE, '<=', searchLimitDate).get();
 
-    querySnapshot.forEach(async (doc) => {
-      const postDocId = doc.id;
-      const userDocId = doc.get(FIELD.USERDOCID);
-      log.debug(
-        `[${doc.id}] post received date : ${doc.get(FIELD.DATE)} / user with doc : <${userDocId}> `,
-      );
+  querySnapshot.forEach(async (doc) => {
+    const postDocId = doc.id;
+    const userDocId = doc.get(FIELD.USERDOCID);
+    log.debug(
+      `[${doc.id}] post received date : ${doc.get(FIELD.DATE)} / user with doc : <${userDocId}> `,
+    );
 
-      sendPostToUser({ postDocId, userDocId });
-      doc.ref.delete();
-    });
-  }
-
- 
+    sendPostToUser({ postDocId, userDocId });
+    doc.ref.delete();
+  });
+}
