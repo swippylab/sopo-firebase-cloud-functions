a34c34eb3b22e0c793864ed2a25e06b965d65973
David <david@swippylab.com>
2022-06-14 17:40:13 +0900

    FIX: apply comment temp call

--- src/user/temp.ts
+++ src/user/temp.ts
@@ -1,38 +1,38 @@
-import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
 import { handlePendingNewPosts } from '../post/pending_new_posts';
 
 // const log = functions.logger;
 
 export const callHandlePendingNewPosts = functions.https.onRequest(async (request, response) => {
-  await handlePendingNewPosts().catch((err) => response.send('fail handle new posts start')); 
+  // eslint-disable-next-line no-unused-vars
+  await handlePendingNewPosts().catch((err) => response.send('fail handle new posts start'));
   response.send('start handle new posts');
 });
 
-export const helloFireStore = functions.https.onRequest(async (request, response) => {
-  admin
-    .firestore()
-    .collection('users')
-    .get()
-    .then((querySnapshot) => {
-      response.send('Hello from FireStore!');
-      querySnapshot.forEach((doc) => {
-        functions.logger.info(doc.data());
-      });
-    });
-});
+// export const helloFireStore = functions.https.onRequest(async (request, response) => {
+//   admin
+//     .firestore()
+//     .collection('users')
+//     .get()
+//     .then((querySnapshot) => {
+//       response.send('Hello from FireStore!');
+//       querySnapshot.forEach((doc) => {
+//         functions.logger.info(doc.data());
+//       });
+//     });
+// });
 
-export const helloStroage = functions.https.onRequest(async (request, response) => {
-  admin
-    .storage()
-    .bucket('gs://swippylab-sopo.appspot.com')
-    .file('users/user1.jpg')
-    .get() //'users/user1.jpg')
-    .then((responseFile) => {
-      response.send('Hello from Storage');
-      functions.logger.info(responseFile[0].name);
-    });
-});
+// export const helloStroage = functions.https.onRequest(async (request, response) => {
+//   admin
+//     .storage()
+//     .bucket('gs://swippylab-sopo.appspot.com')
+//     .file('users/user1.jpg')
+//     .get() //'users/user1.jpg')
+//     .then((responseFile) => {
+//       response.send('Hello from Storage');
+//       functions.logger.info(responseFile[0].name);
+//     });
+// });
 
 // export const queryTest = functions.https.onRequest(async (request, response) => {
 //   const firestore = admin.firestore();
