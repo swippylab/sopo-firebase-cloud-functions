91cd0a2c67f028ff220abab585ae6d7a9b21f984
boogyeom <swippylab.gyeom@gmail.com>
2022-06-16 14:05:47 +0900

    FEATURE: global variables initial value to firestore
    global variable 초기값을 firestore에 실행시 set함

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,22 @@
 import 'dotenv/config';
 import * as admin from 'firebase-admin';
+import { COLLECTION } from './constant/collection';
+import { DOCUMENT } from './constant/document';
+import { FIELD } from './constant/field';
 admin.initializeApp();
+
+///initialize global variables document.
+const globalVariablesInitialValue = {
+  [FIELD.SEARCHFLAG]: false,
+  [FIELD.ISUSINGEXTRA]: false,
+  [FIELD.TOTAlRECEIVABLE]: 0,
+  [FIELD.RECEIVABLECOUNT]: 0,
+};
+
+const sendPostref = admin.firestore().collection(COLLECTION.GLOBALVARIABLES).doc(DOCUMENT.SENDPOST);
+sendPostref.get().then((doc) => {
+  if (!doc.exists) sendPostref.set(globalVariablesInitialValue);
+});
 
 // export * from './post/links';
 export * from './post/new_posts';
