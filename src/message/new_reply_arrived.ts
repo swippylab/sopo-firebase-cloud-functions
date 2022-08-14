c4b9523adfe5441c1281c7325e9de376ba52b2b2
David <david@swippylab.com>
2022-08-14 19:46:03 +0900

    Fix: reply message data add body

--- src/message/new_reply_arrived.ts
+++ src/message/new_reply_arrived.ts
@@ -37,6 +37,7 @@ export default async function sendNewReplyArrivedMessage(
             type: 'newReply',
             postId: postDocId,
             message: title[systemLanguage ?? Language.korean.toString()],
+            body: replyBody,
             // receivedDate: sentDate.toISOString(),
           },
           notification: {
